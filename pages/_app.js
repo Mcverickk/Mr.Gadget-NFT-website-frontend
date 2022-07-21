e12a2a75ce1df2cbb11f6459588bdb25d6fe8940
Chirag Agarwal <85528216+Mcverickk@users.noreply.github.com>
2022-07-21 19:15:01 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -137,7 +137,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>NFT Website</title>
+        <title>dudeNFT</title>
         <link rel="icon" href="/favicon.ico" />
         <style>
           @import
